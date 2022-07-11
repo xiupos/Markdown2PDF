7acc89d2d002e09029c47dffad9820bc66ce488a
xiupos <me@xiupos.net>
2022-07-11 11:34:22 +0900

    add lilypond

--- .config/build.mk
+++ .config/build.mk
@@ -1,22 +1,28 @@
-UID = $(shell id -u)
-GID = $(shell id -g)
+LUID = $(shell id -u)
+LGID = $(shell id -g)
 
 MAIN_MD = main.md
 
-PANDOC = docker-compose build pandoc && \
-	UID=$(UID) GID=$(GID) docker-compose run --rm pandoc
+LATEX = lualatex
+
+PANDOC = LUID=$(LUID) LGID=$(LGID) docker-compose build pandoc && \
+	LUID=$(LUID) LGID=$(LGID) docker-compose run --rm pandoc
 PANDOC_CONFIG = pandoc.yml
 PANDOC_OUT_MAIN = pandoc_out-main.tex
 PANDOC_OUT_TITLE = pandoc_out-title.tex
 PANDOC_TEMPLATE_TITLE = template-title.tex
 
-LATEXMK = docker-compose build latex && \
-	UID=$(UID) GID=$(GID) docker-compose run --rm latex latexmk
-LATEXMK_FLAGS = -pdflatex=lualatex -pdf
+LILYPOND = LUID=$(LUID) LGID=$(LGID) docker-compose build lilypond && \
+	LUID=$(LUID) LGID=$(LGID) docker-compose run --rm lilypond lilypond-book
+LILYPOND_FLAGS = --pdf --latex-program=$(LATEX)
+
+LATEXMK = LUID=$(LUID) LGID=$(LGID) docker-compose build latex && \
+	LUID=$(LUID) LGID=$(LGID) docker-compose run --rm latex latexmk
+LATEXMK_FLAGS = -pdflatex=$(LATEX) -pdf
 LATEXMK_FRAME = frame.tex
 
-PYTHON = docker-compose build python && \
-	UID=$(UID) GID=$(GID) docker-compose run --rm python python
+PYTHON = LUID=$(LUID) LGID=$(LGID) docker-compose build python && \
+	LUID=$(LUID) LGID=$(LGID) docker-compose run --rm python python
 PYTHON_SCRIPT = merger.py
 PYTHON_COVER_PDF = pdf/cover.pdf
 
@@ -30,6 +36,8 @@ $(OUTPUT_COVERED): $(PYTHON_COVER_PDF) $(OUTPUT)
 	$(PYTHON) $(PYTHON_SCRIPT) $^ $@
 
 $(OUTPUT): $(PANDOC_OUT_MAIN) $(PANDOC_OUT_TITLE)
+	mv $(PANDOC_OUT_MAIN) $(PANDOC_OUT_MAIN:.tex=.lytex)
+	$(LILYPOND) $(LILYPOND_FLAGS) $(PANDOC_OUT_MAIN:.tex=.lytex)
 	$(LATEXMK) $(LATEXMK_FLAGS) $(LATEXMK_FRAME)
 	mv $(LATEXMK_FRAME:.tex=.pdf) $@
 
@@ -40,7 +48,8 @@ $(PANDOC_OUT_TITLE): $(MAIN_MD)
 		$^
 
 $(PANDOC_OUT_MAIN): $(MAIN_MD)
-	$(PANDOC) --filter pandoc-crossref \
+	$(PANDOC) -f markdown+raw_tex \
+		--filter pandoc-crossref \
 		--top-level-division=section \
 		-M "crossrefYaml=$(PANDOC_CONFIG)" \
 		-o $@ \
